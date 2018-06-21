cc7a83c75d785b87e1729b1bc9b6f0055535ce0a
Бабин Анатолий Николаевич <ababin2@alfabank.ru>
2018-06-21 23:41:24 +0300

    feat: add jquery && html structure

---
+++ src/slider.js
@@ -0,0 +1,16 @@
+/*
+* Custom slider
+->  pass element to create slider
+    then initialize html structure with next / prev button
+    run autoslide
+    show progress bar
+*/
+
+
+class Slider {
+
+    createSlider() {
+        console.log('slider is created');
+    }
+
+};
